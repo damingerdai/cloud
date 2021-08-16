5b1aed431fac3662da0fb39717e560fbe5481246
damingerdai <mingguobin@live.com>
2021-08-16 22:24:45 +0800

    feat: add hystrix support

--- service-ribbon/src/main/java/org/daming/service/ribbon/HelloService.java
+++ service-ribbon/src/main/java/org/daming/service/ribbon/HelloService.java
@@ -1,5 +1,6 @@
 package org.daming.service.ribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,9 +10,14 @@ public class HelloService {
     // private RestTemplate restTemplate;
     private HelloFeignClient helloFeignClient;
 
+    @HystrixCommand(fallbackMethod = "hiServiceFallback")
     public String hiService(String name) {
         // return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
         return this.helloFeignClient.hi(name);
+    }
+
+    public String hiServiceFallback(String name) {
+        return "error:\t" + name;
     }
 
 
