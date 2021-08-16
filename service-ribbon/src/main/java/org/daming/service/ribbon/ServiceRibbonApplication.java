4038f7c6de06edc5ecad3e0ce9e8d5ef8e093c82
damingerdai <mingguobin@live.com>
2021-08-16 22:52:43 +0800

    feat: add hystrix dashboard

--- service-ribbon/src/main/java/org/daming/service/ribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/org/daming/service/ribbon/ServiceRibbonApplication.java
@@ -1,11 +1,14 @@
 package org.daming.service.ribbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -14,11 +17,23 @@ import org.springframework.web.client.RestTemplate;
 // @EnableEurekaClient
 @EnableFeignClients
 @EnableHystrix
+@EnableHystrixDashboard
 //@EnableDiscoveryClient
 public class ServiceRibbonApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);  //系统启动时加载顺序
+        registrationBean.addUrlMappings("/hystrix.stream");//路径
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 //    @Bean
